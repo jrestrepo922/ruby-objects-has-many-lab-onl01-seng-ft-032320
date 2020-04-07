5369c7ee698017a73681c75e3a7459cffe9adab6
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-07 16:08:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,16 @@
 class Song # a song must have an artist 
- attr_accessor :artist  
+ attr_accessor :artist, :name 
+ 
+ @@all = []
+ 
+ def initialize(name)
+   @name = name 
+   @@all << self 
+ end 
+ 
+ def self.all 
+   @@all 
+ end 
+ 
+ 
 end 
