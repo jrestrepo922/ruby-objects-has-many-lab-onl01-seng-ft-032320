12195c4e42595fd9dfedf5ae48df0ab7d859a38e
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-07 16:20:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,9 +10,15 @@ class Artist #artist should have many songs
   end 
   #In order to have an artist find all of it's songs, the song class needs to know about all its song instances and a song instance needs to know about the artist class it belongs to.
   
-  def add_song_by_name(name)#need to provide argument since it will create a new song and the initialize method requires this argument to create the song.
-    song = Song.new(name) #this is now an object 
-    song.artist = self # setting the instance of the song class setter method artist equals to the instance of the object. This associates both of the objects.
+  def add_song(song)
+    song.artist = self 
+  end 
+  
+  
+  
+  def add_song_by_name(name)     #need to provide argument since it will create a new song and the  initialize method requires this argument to create the song.
+    song = Song.new(name)    #this is now an object 
+    song.artist = self     #setting the instance of the song class setter method artist equals to the instance of the object. This associates both of the objects.
     
   end 
 end 
