b2c7ff2863107ec519b8a4b65df375c110d602e7
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-07 16:12:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,10 @@
 class Artist #artist should have many songs 
   attr_accessor :name 
   
-  def songs # return the songs that belong to the instanct of this artist
-    
+  def songs # return the songs that belong to the instant of this artist
+    Song.self.select { |song| song.artist == self}
   end 
+  #
 end 
 
 
